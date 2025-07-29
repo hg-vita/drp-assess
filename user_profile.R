a963cbed180b38b62cb80f33dcb73c3110f974a2
alex.929 <189745914+hg-vita@users.noreply.github.com>
2025-07-29 13:54:28 +0530

    updated user_profile

--- user_profile.R
+++ user_profile.R
@@ -1,31 +1,21 @@
-
 #-------------------------------------------------------------------
 # Project: Population-Weighted Health Coverage
 # Script: User Profile Configuration
-# Author: [Your Name]
+# Author: XXXX
 # Date: 28 July 2025
 #-------------------------------------------------------------------
 
-# Set working directories and environment-agnostic paths
-# This profile should be loaded before running any other script
 
-USERNAME <- Sys.getenv("USERNAME")
-USER     <- Sys.getenv("USER")
-
-# Define the project folder based on user
-if (USER %in% c("karavan88", "your_unix_user")) {
-  projectFolder <- file.path("/Users", USER, "Documents", "GitHub", "consultancy-assessment")
-} else {
-  projectFolder <- getwd()  # fallback to current directory
-}
+# Default to current working directory as the project folder
+projectFolder <- getwd()
 
 # Confirm that the main directory is correct
 stopifnot(dir.exists(projectFolder))
 
 # Set up key folders
-dataDir     <- file.path(projectFolder, "data")
+dataDir     <- file.path(projectFolder, "01_data")
 outputDir   <- file.path(projectFolder, "outputs")
-scriptsDir  <- file.path(projectFolder, "scripts")
+scriptsDir  <- file.path(projectFolder, "02_script")
 reportDir   <- file.path(projectFolder, "report")
 
 # Validate that key folders exist
